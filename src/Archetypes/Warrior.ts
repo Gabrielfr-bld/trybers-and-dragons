51cf00a85f549e961d180a496679f7e08ac76431
gabriel <franklingabriel158@gmail.com>
2022-03-10 22:25:59 -0300

    Cria classe Warrior

---
+++ src/Archetypes/Warrior.ts
@@ -0,0 +1,23 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+class Warrior extends Archetype {
+  private static _warriorInstances = 0;
+  public energyType: EnergyType;
+
+  constructor(name:string) {
+    super(name);
+    this.energyType = 'mana';
+    Warrior.incrementWarriorInstance();
+  }
+
+  private static incrementWarriorInstance() {
+    Warrior._warriorInstances += 1;
+  }
+
+  public static createdArtchetypeInstances(): number {
+    return Warrior._warriorInstances;
+  }
+}
+
+export default Warrior;
