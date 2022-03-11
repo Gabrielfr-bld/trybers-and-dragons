1542a5592f8a2c59feaa7cc811ff3871eac60f52
gabriel <franklingabriel158@gmail.com>
2022-03-10 22:20:41 -0300

    Cria classe Mage

---
+++ src/Archetypes/Mage.ts
@@ -0,0 +1,23 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+class Mage extends Archetype {
+  private static _mageInstances = 0;
+  public energyType: EnergyType;
+
+  constructor(name:string) {
+    super(name);
+    this.energyType = 'mana';
+    Mage.incrementMageInstance();
+  }
+
+  private static incrementMageInstance() {
+    Mage._mageInstances += 1;
+  }
+
+  public static createdArtchetypeInstances(): number {
+    return Mage._mageInstances;
+  }
+}
+
+export default Mage;
