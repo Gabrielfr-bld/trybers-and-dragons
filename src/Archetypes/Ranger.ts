e35ee68df2cbe74ed8ee0644bcd4f240aa900630
gabriel <franklingabriel158@gmail.com>
2022-03-10 22:22:12 -0300

    Cria classe Ranger

---
+++ src/Archetypes/Ranger.ts
@@ -0,0 +1,23 @@
+import { EnergyType } from '../Energy';
+import Archetype from './Archetype';
+
+class Ranger extends Archetype {
+  private static _rangerInstances = 0;
+  public energyType: EnergyType;
+
+  constructor(name:string) {
+    super(name);
+    this.energyType = 'stamina';
+    Ranger.incrementRangerInstance();
+  }
+
+  private static incrementRangerInstance() {
+    Ranger._rangerInstances += 1;
+  }
+
+  public static createdArtchetypeInstances(): number {
+    return Ranger._rangerInstances;
+  }
+}
+
+export default Ranger;
